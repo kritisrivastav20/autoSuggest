3ab6ddf51e7a5dc3ac21b7e7549c8f43e8b71d11
kritisrivastav20 <kriti.vabby15@gmail.com>
2020-11-26 17:24:15 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  constructor(props) {
+		super(props);
+		this.state = {
+      results: [],
+      value: '',
+			countries: ['India', 'Japan', 'Australia', 'United States', 'Germany', 'France', 'Spain', 'Turkey', 'Isreal'],
+		};
+  }
+  
+  onChange = (event) => {
+    const matches = this.state.countries.filter(item => item.includes(event.target.value));    
+    this.setState(
+      {
+        results: matches,
+        value: event.target.value,
+      }
+    )
+  }
+  
+  handleClick = () => {
+    alert(this.state.results.length + ' records found!');
+  }
+
+
+  render() {
+
+    const { results, value } = this.state;
+
+  	return (
+			<div className="header">
+				<h1>Start search</h1>
+				<input placeholder="Ex: Add your text here..." onChange={this.onChange}/>
+				<button onClick={this.handleClick}>+</button>
+
+        {value && !results && <div>No records found...</div>}
+				
+        { results && value && 
+        results.map((item, id) =>
+        <div key = {id}>
+          <ul className="resultItem">{item}</ul>
+        </div>
+        )
+        }
+			</div>
+		);
+}
 }
 
-export default App;
